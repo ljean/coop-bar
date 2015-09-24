3b7fae26c0e4c999fa1736f417b49d3983231364
Luc Jean <ljean@apidev.fr>
2015-09-24 14:46:45 +0200

    minor changes

--- coop_bar/__init__.py
+++ coop_bar/__init__.py
@@ -2,8 +2,9 @@
 
 VERSION = (1, 2, 0)
 
+
 def get_version():
+    """version"""
     return '%s.%s.%s' % (VERSION[0], VERSION[1], VERSION[2])
 
 __version__ = get_version()
-
--- coop_bar/urls.py
+++ coop_bar/urls.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import patterns, include, url
+
+from django.conf.urls import patterns
 
 from coop_bar.bar import CoopBar
-bar = CoopBar() #This is a Borg
 
-urlpatterns = patterns('',
-)
+
+bar = CoopBar()  # This is a Borg
+
+urlpatterns = patterns('')
